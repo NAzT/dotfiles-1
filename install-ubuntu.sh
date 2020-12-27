cf53e64cc63d0e60fd4753bd8566430b60dd8cf0
Nat Weerawan <nat.wrw@gmail.com>
2020-12-27 13:10:31 +0700

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -16,41 +16,41 @@ sudo apt-get install --yes ansible
 # install git so ansible-dotfiles can be cloned
 sudo apt-get install --yes git
 
-# clone repository if not already cloned
-if [ -d ~/ansible-dotfiles ]; then
-  echo "$HOME/ansible-dotfiles already cloned! Make sure it is a clone of: http://github.com/dustinspecker/ansible-dotfiles.git"
-else
-  git clone http://github.com/dustinspecker/ansible-dotfiles.git ~/ansible-dotfiles
-fi
+# # clone repository if not already cloned
+# if [ -d ~/ansible-dotfiles ]; then
+#   echo "$HOME/ansible-dotfiles already cloned! Make sure it is a clone of: http://github.com/dustinspecker/ansible-dotfiles.git"
+# else
+#   git clone http://github.com/dustinspecker/ansible-dotfiles.git ~/ansible-dotfiles
+# fi
 
-cd ~/ansible-dotfiles
+# cd ~/ansible-dotfiles
 
-# update origin references in case it is stale
-git fetch
+# # update origin references in case it is stale
+# git fetch
 
-# do not exit if following commands return non-zero exit code
-# want to store and use these exit codes
-set +e
+# # do not exit if following commands return non-zero exit code
+# # want to store and use these exit codes
+# set +e
 
-# only update local git if no local changes uncommitted
-(git --no-pager diff --quiet --exit-code)
-local_changed_files=$?
+# # only update local git if no local changes uncommitted
+# (git --no-pager diff --quiet --exit-code)
+# local_changed_files=$?
 
-# only update local git if local git is not ahead of master
-(($(git rev-list --right-only --count origin/master..master) == 0))
-ahead_of_origin_master=$?
+# # only update local git if local git is not ahead of master
+# (($(git rev-list --right-only --count origin/master..master) == 0))
+# ahead_of_origin_master=$?
 
-# go back to exiting if any command fails
-set -e
+# # go back to exiting if any command fails
+# set -e
 
-# update local git if zero local changes
-if [[ ${local_changed_files} -eq 0 && ${ahead_of_origin_master} -eq 0 ]]; then
-  git reset --hard origin/master
-else
-  echo 'Local git has changes or is ahead of origin/master, so not updating local git.'
-fi
+# # update local git if zero local changes
+# if [[ ${local_changed_files} -eq 0 && ${ahead_of_origin_master} -eq 0 ]]; then
+#   git reset --hard origin/master
+# else
+#   echo 'Local git has changes or is ahead of origin/master, so not updating local git.'
+# fi
 
-# execute playbook to setup development environment
-ansible-playbook -i hosts dev-env.yml --ask-become-pass "$@"
+# # execute playbook to setup development environment
+# ansible-playbook -i hosts dev-env.yml --ask-become-pass "$@"
 
-echo 'Log out and back in for shell changes to take effect'
+# echo 'Log out and back in for shell changes to take effect'
